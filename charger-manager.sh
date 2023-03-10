dc2b8976952375158054275633d8541dc403fd1d
Szybet <53944559+Szybet@users.noreply.github.com>
2023-03-10 18:51:32 +0100

    nevermind

--- charger-manager.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# to be launched from IPD
-fbgrab /tmp/background.png
-env QT_QPA_PLATFORM=kobo chroot /kobo /usr/local/bin/charger-controller.bin
-fbink -g file=/tmp/background.png
-rm /tmp/background
