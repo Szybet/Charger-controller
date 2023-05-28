0666fbb2bbf1969a92c288add5722fc1b5cdaf4c
Szybet <53944559+Szybet@users.noreply.github.com>
2023-05-28 21:49:13 +0200

    Update charger_manager.sh

--- charger_manager.sh
+++ charger_manager.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 fbgrab /tmp/background.png
-env QT_QPA_PLATFORM=kobo chroot /kobo /usr/local/bin/charger-controller.bin
+env QT_QPA_PLATFORM=kobo chroot /kobo /usr/local/bin/charger_manager.bin
 fbink -g file=/tmp/background.png
